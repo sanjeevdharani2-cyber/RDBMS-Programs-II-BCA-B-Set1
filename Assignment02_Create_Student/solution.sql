f63ade7a17dc08b3ea1b8dfdd8b1d507f6405d3d
sanjeevdharani2-cyber <sanjeevdharani2@gmail.com>
2026-09-22 12:19:46 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,13 +1,10 @@
--- Create Student table
-
--- StudentID
-
--- StudentName
-
--- DOB
-
--- Gender
-
--- DepartmentID
-
--- Add constraints
+CREATE TABLE Student (
+    StudentID NUMBER(5) PRIMARY KEY,
+    StudentName VARCHAR2(20) NOT NULL,
+    DOB DATE,
+    Gender VARCHAR2(10),
+    DepartmentID NUMBER(5) NOT NULL,
+    CONSTRAINT fk_student_department
+        FOREIGN KEY (DepartmentID)
+        REFERENCES Department(DepartmentID)
+);
