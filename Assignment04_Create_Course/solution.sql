6395ec6837447d5da8cea8c9bb6f76aaf1d5117d
sanjeevdharani2-cyber <sanjeevdharani2@gmail.com>
2026-09-22 12:22:30 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,15 @@
--- Create Course table
+CREATE TABLE Course (
+    CourseID NUMBER(5) PRIMARY KEY,
+    CourseName VARCHAR2(30) NOT NULL,
+    Credits NUMBER(2),
+    DepartmentID NUMBER(5),
+    CONSTRAINT fk_course_department
+        FOREIGN KEY (DepartmentID)
+        REFERENCES Department(DepartmentID)
+);
+SELECT * FROM Course;
+DESCRIBE Department;
 
--- Insert three records
+DESCRIBE Student;
 
--- Display structure
+DESCRIBE Course;
