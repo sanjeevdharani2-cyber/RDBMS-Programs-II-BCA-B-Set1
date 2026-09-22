4177ab465c3816166a2ff01ea73754ab6afcead6
sanjeevdharani2-cyber <sanjeevdharani2@gmail.com>
2026-09-22 12:23:56 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,9 +1,12 @@
--- Insert
+INSERT INTO Student
+(StudentID, StudentName, Gender, DepartmentID)
+VALUES (1001, 'Arun', 'Male', 101);
 
--- 1001 Arun
+INSERT INTO Student
+(StudentID, StudentName, Gender, DepartmentID)
+VALUES (1002, 'Divya', 'Female', 102);
 
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
+INSERT INTO Student
+(StudentID, StudentName, Gender, DepartmentID)
+VALUES (1003, 'Karthik', 'Male', 101);
+SELECT * FROM Student;
