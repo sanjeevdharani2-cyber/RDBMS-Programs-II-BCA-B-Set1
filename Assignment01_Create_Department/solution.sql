9c68ca7034c44f5e84c98d70b527b3db501b28fe
sanjeevdharani2-cyber <sanjeevdharani2@gmail.com>
2026-09-22 12:18:26 +0530

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,7 +1,5 @@
--- Write your SQL here
-
--- Create Database
-
--- Use Database
-
--- Create Department Table
+CREATE TABLE Department (
+    DepartmentID NUMBER(5) PRIMARY KEY,
+    DepartmentName VARCHAR2(20) NOT NULL,
+    HOD VARCHAR2(20)
+);
